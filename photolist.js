c9284b22d7445c89ee526fe170a16aab20b201da
Gerly Kaytukov <gerlyecc@yahoo.com>
2022-11-10 10:51:55 -0500

    IG Clone React Native

---
+++ photolist.js
@@ -0,0 +1,51 @@
+// Put a fake a list of photos instead of hitting an API
+
+const photolist = [
+    {
+        id: 1, 
+        photoURL: 'https://images.unsplash.com/photo-1665655277013-bf72b3bccd9b?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTY2ODA5MTI3MQ&ixlib=rb-1.2.1&q=80&w=1080',
+        description: 'Beautiful Lounge Chair',
+        userName: 'Mustard Chair', 
+        profilePhoto: 'https://randomuser.me/api/portraits/men/1.jpg',
+    },
+
+
+    {
+        id: 2,
+        photoURL: 'https://images.unsplash.com/photo-1666668973700-e45696248d3f?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTY2ODA5MTUzMg&ixlib=rb-4.0.3&q=80&w=1080',
+        description: 'Beautiful Mountain View',
+        userName: 'Mountains', 
+        profilePhoto: 'https://randomuser.me/api/portraits/women/2.jpg',
+    },
+    {
+        id: 3,
+        photoURL: 'https://images.unsplash.com/photo-1666878462408-74bae9b118eb?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTY2ODA5MTU5Mw&ixlib=rb-4.0.3&q=80&w=1080',
+        description: 'Business man rushing to meeting at a hotel',
+        userName: 'Business Man', 
+        profilePhoto: 'https://randomuser.me/api/portraits/men/3.jpg',
+    },
+    {
+        id: 4,
+        photoURL: 'https://images.unsplash.com/photo-1667327484093-e16708f31790?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTY2ODA5MTY1OA&ixlib=rb-4.0.3&q=80&w=1080',
+        description: 'Lady in pink to not mess with',
+        userName: 'Gangsta Girl', 
+        profilePhoto: 'https://randomuser.me/api/portraits/women/4.jpg',
+    },
+    {
+        id: 5,
+        photoURL: 'https://images.unsplash.com/photo-1665860310121-02a15af7c0f6?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTY2ODA5MTY5Ng&ixlib=rb-1.2.1&q=80&w=1080',
+        description: 'Gorgeous sunset',
+        userName: 'Sunset', 
+        profilePhoto: 'https://randomuser.me/api/portraits/men/5.jpg',
+    },
+    {
+        id: 6,
+        photoURL: 'https://images.unsplash.com/photo-1666375720094-a8a3c602e43c?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTY2ODA5MTcyNw&ixlib=rb-4.0.3&q=80&w=1080',
+        description: 'Samurai Girl ready to fight',
+        userName: 'Samurai Girl', 
+        profilePhoto: 'https://randomuser.me/api/portraits/women/6.jpg',
+    },
+
+]
+
+export default photolist; 
