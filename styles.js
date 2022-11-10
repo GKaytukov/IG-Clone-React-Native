28888d3a99cec50c1c41cf37dec309e92da22edb
Gerly Kaytukov <gerlyecc@yahoo.com>
2022-11-10 11:54:07 -0500

    details page and adding navigation

--- styles.js
+++ styles.js
@@ -2,7 +2,7 @@ import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
     feed: {
-        marginTop: 52,
+        // marginTop: 52,
         flex: 1,
         backgroundColor: 'white',
     }, 
